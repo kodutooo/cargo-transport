b1ccc8da51ced203d532145d6ceeac3643b8ca79
kodutooo <46077112+kodutooo@users.noreply.github.com>
2019-01-05 20:41:14 +0300

    add form component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PageHeader from '../PageHeader';
 import Table from '../Table';
+import Form from '../Form';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <PageHeader />
         <Table />
+        <Form />
       </div>
     );
   }
