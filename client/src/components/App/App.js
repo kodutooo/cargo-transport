67a27d8975e264a78462d63422de4cb67a9ee24a
kodutooo <46077112+kodutooo@users.noreply.github.com>
2019-01-05 20:14:49 +0300

    add simple page header

---
+++ client/src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PageHeader from '../PageHeader';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PageHeader />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/PageHeader/PageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageHeader.css';
+
+const pageHeader = () => {
+  return(
+    <header className='page-header'>
+      <h1>CARGO TRANSPORT INTERNATIONAL</h1>
+    </header>
+  );
+};
+
+export default pageHeader;
