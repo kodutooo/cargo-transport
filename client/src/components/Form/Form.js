7fa270eb80d0cd2a980051a759322180c2799d96
kodutooo <46077112+kodutooo@users.noreply.github.com>
2019-01-05 20:51:16 +0300

    add select status to form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Select from '../Elements/Select';
+import { STATUS_OPTIONS } from '../../config';
 
 class Form extends Component {
   render() {
     return (
       <form>
-
+        <Select options={STATUS_OPTIONS} />
       </form>
     );
   };
---
+++ client/src/config/index.js
@@ -0,0 +1,1 @@
+export const STATUS_OPTIONS = ['Pending', 'In transit', 'Arrived'];
