5349511e4b07c3b1863afda3f9b0e83c79378ff5
kodutooo <46077112+kodutooo@users.noreply.github.com>
2019-01-07 19:36:26 +0300

    add custom validator to shipment model

--- server/models/shipment.js
+++ server/models/shipment.js
@@ -17,7 +17,16 @@ const shipmentSchema = new Schema({
   shippedOn: {
     type: Date,
     default: Date.now(),
-    required: () => this.status !== 'Pending'
+    required: () => this.status !== 'Pending',
+    validate: {
+      validator: function(value) {
+        if (this.status === 'Pending') {
+          return value === null;
+        }
+        return !Number.isNaN(Date.parse(value));
+      },
+      msg: 'Date is invalid'
+    }
   }
 });
 
