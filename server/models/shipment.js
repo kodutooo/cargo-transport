09614a4b2afdce3558322de00133afb46d9bbf47
kodutooo <46077112+kodutooo@users.noreply.github.com>
2019-01-07 20:34:07 +0300

    add handling shipment socket to server

--- server/models/shipment.js
+++ server/models/shipment.js
@@ -1,4 +1,4 @@
-const mongoose, { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const shipmentSchema = new Schema({
   status: {
--- server/sockets/shipment.js
+++ server/sockets/shipment.js
@@ -1,10 +1,37 @@
 const socketIo = require('socket.io');
+const Shipment = require('../models/shipment');
 
 function attachSocket(server) {
   const io = socketIo(server);
   io.on('connection', socket => {
-    socket.on('new shipment', shipment => console.log(shipment));
+    handleNewConnection(socket);
+    socket.on('new shipment', shipment =>{
+      handleNewShipment(shipment, socket, io);
+    });
   });
 };
+
+function handleNewConnection(socket) {
+  Shipment.find((err, docs) => {
+    if (err) {
+      return console.error(err);
+    }
+    socket.emit('list of shipments', docs);
+  })
+}
+
+function handleNewShipment(shipment, socket, io) {
+  const newShipment = new Shipment(shipment);
+  newShipment.save(err => {
+    if (err) {
+      return handleError(err, socket);
+    }
+    io.emit('new shipment', newShipment.toObject());
+  });
+};
+
+function handleError(err, socket){
+  socket.emit('error', err);
+}
 
 module.exports = { attachSocket };
